1276f567db12830104f93f8d83316a79236c9a47
shervin <shervinrajabi2016@gmail.com>
2025-05-29 13:23:12 +0330

    add methods to complete custom linked list.

--- src/datastructures/lists/CustomLinkedList.java
+++ src/datastructures/lists/CustomLinkedList.java
@@ -1,7 +1,9 @@
 package datastructures.lists;
 import datastructures.interfaces.LinkedList;
 
-public class CustomLinkedList<T> implements LinkedList<T>{
+import java.util.Objects;
+
+public class CustomLinkedList<T> implements LinkedList<T> {
     private static class Node<T> {
         T data;
         Node<T> next;
@@ -15,6 +17,164 @@ public class CustomLinkedList<T> implements LinkedList<T>{
     private Node<T> head, tail;
     private int size = 0;
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public void addFirst(T t) {
+        Node<T> newNode = new Node<>(t);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
 
+    @Override
+    public void addLast(T t) {
+        Node<T> newNode = new Node<>(t);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        T data = head.data;
+        head = head.next;
+        size--;
+        if (isEmpty()) {
+            tail = null;
+        } else {
+            head.prev = null;
+        }
+        return data;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        T data = tail.data;
+        tail = tail.prev;
+        size--;
+        if (isEmpty()) {
+            head = null;
+        } else {
+            tail.next = null;
+        }
+        return data;
+    }
+
+    @Override
+    public T getFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        return head.data;
+    }
+
+    @Override
+    public T getLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        return tail.data;
+    }
+
+    @Override
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public Object get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        @SuppressWarnings("unchecked")
+        T newElement = (T) element;
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        T oldData = current.data;
+        current.data = newElement;
+        return oldData;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        T element = (T) o;
+        addLast(element);
+        return true;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        Node<T> current = head;
+        while (current != null) {
+            if (Objects.equals(o, current.data)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        Node<T> current = head;
+        while (current != null) {
+            if (Objects.equals(o, current.data)) {
+                if (current == head) {
+                    removeFirst();
+                } else if (current == tail) {
+                    removeLast();
+                } else {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                    size--;
+                }
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
 }
