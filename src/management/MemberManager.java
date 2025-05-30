70f993a7da8aedba406039827d27dbd611f0208d
shervin <shervinrajabi2016@gmail.com>
2025-05-30 14:29:17 +0330

    completed MemberManager class methods.

--- src/management/MemberManager.java
+++ src/management/MemberManager.java
@@ -3,28 +3,53 @@ package management;
 import library.Member;
 import library.Transaction;
 
+import java.util.HashMap;
+
 public class MemberManager {
     // TODO: Define a data structure that stores memberIds and their matching member objects
-
+    private HashMap<String, Member> members;
     public MemberManager() {
-        // TODO: Initialize your data structure here
+        members = new HashMap<>();
     }
 
     public void addMember(Member member) {
-        // TODO: Add member and their id to your data structure
+        if (member == null) {
+            return;
+        }
+        members.put(member.getMemberId(), member);
     }
 
     public Member getMember(String memberId) {
-        // TODO
-        return null;
+        if (memberId == null) {
+            System.out.println("Member id is null");
+            return null;
+        }
+        return members.get(memberId);
     }
 
     public void recordTransaction(String memberId, Transaction transaction) {
-        // TODO
+        if (memberId == null) {
+            System.out.println("Member id is null");
+            return;
+        }
+        Member member = getMember(memberId);
+        if (member == null) {
+            System.out.println("Member not found");
+            return;
+        }
+        member.addTransaction(transaction);
     }
 
     public Transaction getLastTransaction(String memberId) {
-        // TODO
-        return null;
+        if (memberId == null) {
+            System.out.println("Member id is null");
+            return null;
+        }
+        Member member = getMember(memberId);
+        if (member == null) {
+            System.out.println("Member not found");
+            return null;
+        }
+        return member.getLastTransaction();
     }
 }
